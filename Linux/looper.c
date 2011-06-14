d4affb21b1278ff83da62dec9b5354d2ecb4dc09
Enrique Arango Lyons <karlysw@gmail.com>
2011-06-14 01:46:12 -0500

    now working, and receving file in params

--- Linux/looper.c
+++ Linux/looper.c
@@ -12,6 +12,10 @@
 #include <linux/hdreg.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Santiago Achury Jaramillo");
+MODULE_AUTHOR("Enrique Arango Lyons");
+MODULE_AUTHOR("Juan Camilo Cardona Cano");
+MODULE_DESCRIPTION("Mount an image file as a dev node");
 
 static int major_num = 0;
 module_param(major_num, int, 0);
@@ -19,9 +23,11 @@ static int logical_block_size = 512;
 module_param(logical_block_size, int, 0);
 static int nsectors = 1024; /* How big the drive is */
 module_param(nsectors, int, 0);
-static char* filename = "/home/ubuntu/Downloads/ubuntu-11.04-desktop-i386.iso";
+static char* filename = NULL;
 module_param(filename, charp, 0000);
 MODULE_PARM_DESC(filename, "Image filename to mount");
+
+static struct file* filp;
 
 #define KERNEL_SECTOR_SIZE 512
 
@@ -34,20 +40,23 @@ static struct request_queue *Queue;
  * The internal representation of our device.
  */
 static struct looper_device {
-	unsigned long size;
-	spinlock_t lock;
-	u8 *data;
-	struct gendisk *gd;
+  unsigned long size;
+  spinlock_t lock;
+  u8 *data;
+  struct gendisk *gd;
 } Device;
+
 
 /*
  * Open file
  */
 struct file* file_open(const char* path, int flags, int rights) {
-  struct file* filp = NULL;
+  
   mm_segment_t oldfs;
   int err = 0;
-
+  
+  printk(KERN_INFO "looper: opening file");
+  
   oldfs = get_fs();
   set_fs(get_ds());
   filp = filp_open(path, flags, rights);
@@ -62,6 +71,7 @@ struct file* file_open(const char* path, int flags, int rights) {
  * Close file
  */
 void file_close(struct file* filp) {
+  printk(KERN_INFO "looper: closing file");
   filp_close(filp, NULL);
 }
 
@@ -69,9 +79,12 @@ void file_close(struct file* filp) {
  * Read file
  */
 int file_read(struct file* file, unsigned long long offset, unsigned char* data, unsigned int size) {
+
   mm_segment_t old_fs;
   int ret;
   
+  printk(KERN_INFO "looper: reading file");  
+
   old_fs = get_fs();
   set_fs(get_ds());
   
@@ -87,6 +100,8 @@ int file_read(struct file* file, unsigned long long offset, unsigned char* data,
 int file_write(struct file* file, unsigned long long offset, unsigned char* data, unsigned int size) {
   mm_segment_t oldfs;
   int ret;
+
+  printk(KERN_INFO "looper: writing file");
   
   oldfs = get_fs();
   set_fs(get_ds());
@@ -102,26 +117,30 @@ int file_write(struct file* file, unsigned long long offset, unsigned char* data
  */
 static void looper_transfer(struct looper_device *dev, sector_t sector,
 			 unsigned long nsect, char *buffer, int write) {
-	unsigned long offset = sector * logical_block_size;
-	unsigned long nbytes = nsect * logical_block_size;
-	struct file* filp = NULL;
-
-	if ((offset + nbytes) > dev->size) {
-		printk (KERN_NOTICE "looper: Beyond-end write (%ld %ld)\n", offset, nbytes);
-		return;
-	}
-	filp = file_open(filename, 0, 0);
-	
-	if (write)  	  
-	  file_write(filp, offset, buffer, nbytes); 
-	else	 
-	  file_read(filp, offset, buffer, nbytes);
-	file_close(filp);
+ 
+  unsigned long offset = sector * logical_block_size;
+  unsigned long nbytes = nsect * logical_block_size;
+  
+  printk(KERN_INFO "looper: executing transfer");
+ 
+  if ((offset + nbytes) > dev->size) {
+    printk (KERN_NOTICE "looper: Beyond-end write (%ld %ld)\n", offset, nbytes);
+    return;
+  }
+  //filp = file_open(filename, 0, 0);	
+  if (write)  	  
+    file_write(filp, offset, buffer, nbytes); 
+  else	 
+    file_read(filp, offset, buffer, nbytes);
+  //file_close(filp);
 }
 
 static void looper_request(struct request_queue *q) {
+ 
   struct request *req;
-  
+ 
+  printk(KERN_INFO "looper: executing request");
+   
   req = blk_fetch_request(q);
   while (req != NULL) {
     if (req == NULL || (req->cmd_type != REQ_TYPE_FS)) {
@@ -143,80 +162,104 @@ static void looper_request(struct request_queue *q) {
  * use tools such as fdisk to partition the drive otherwise.
  */
 int looper_getgeo(struct block_device * block_device, struct hd_geometry * geo) {
-	long size;
+  
+  long size;
+  
+  printk(KERN_INFO "looper: executing getgeo");
 
-	/* We have no real geometry, of course, so make something up. */
-	size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
-	geo->cylinders = (size & ~0x3f) >> 6;
-	geo->heads = 4;
-	geo->sectors = 16;
-	geo->start = 0;
-	return 0;
+  /* We have no real geometry, of course, so make something up. */
+  size = Device.size * (logical_block_size / KERNEL_SECTOR_SIZE);
+  geo->cylinders = (size & ~0x3f) >> 6;
+  geo->heads = 4;
+  geo->sectors = 16;
+  geo->start = 0;
+  return 0;
+}
+
+
+static int looper_open(struct block_device *bdev, fmode_t mode) {
+  printk(KERN_INFO "looper: executing open");
+  filp = file_open(filename, 0, 0);
+  return 0;
+}
+
+static int looper_release(struct gendisk *gd, fmode_t mode) {
+  printk(KERN_INFO "looper: executing close");
+  file_close(filp);
+  return 0;
 }
 
 /*
  * The device operations structure.
  */
 static struct block_device_operations looper_ops = {
-		.owner  = THIS_MODULE,
-		.getgeo = looper_getgeo
+  .owner  = THIS_MODULE,
+  .open = looper_open,
+  .release = looper_release,
+  .getgeo = looper_getgeo
 };
 
 static int __init looper_init(void) {
-	/*
-	 * Set up our internal device.
-	 */
-	Device.size = nsectors * logical_block_size;
-	spin_lock_init(&Device.lock);
-	Device.data = vmalloc(Device.size);
-	if (Device.data == NULL)
-		return -ENOMEM;
-	/*
-	 * Get a request queue.
-	 */
-	Queue = blk_init_queue(looper_request, &Device.lock);
-	if (Queue == NULL)
-		goto out;
-	blk_queue_logical_block_size(Queue, logical_block_size);
-	/*
-	 * Get registered.
-	 */
-	major_num = register_blkdev(major_num, "looper");
-	if (major_num <= 0) {
-		printk(KERN_WARNING "looper: unable to get major number\n");
-		goto out;
-	}
-	/*
-	 * And the gendisk structure.
-	 */
-	Device.gd = alloc_disk(16);
-	if (!Device.gd)
-		goto out_unregister;
-	Device.gd->major = major_num;
-	Device.gd->first_minor = 0;
-	Device.gd->fops = &looper_ops;
-	Device.gd->private_data = &Device;
-	strcpy(Device.gd->disk_name, "looper0");
-	set_capacity(Device.gd, nsectors);
-	Device.gd->queue = Queue;
-	add_disk(Device.gd);
+  
+  if (filename == NULL) {
+    printk(KERN_WARNING "looper: no filename defined");
+    return -1;
+  }
 
-	return 0;
-
-out_unregister:
-	unregister_blkdev(major_num, "looper");
-out:
-	vfree(Device.data);
-	return -ENOMEM;
+  /*
+   * Set up our internal device.
+   */
+  Device.size = nsectors * logical_block_size;
+  spin_lock_init(&Device.lock);
+  Device.data = vmalloc(Device.size);
+  if (Device.data == NULL)
+    return -ENOMEM;
+  /*
+   * Get a request queue.
+   */
+  Queue = blk_init_queue(looper_request, &Device.lock);
+  if (Queue == NULL)
+    goto out;
+  blk_queue_logical_block_size(Queue, logical_block_size);
+  /*
+   * Get registered.
+   */
+  major_num = register_blkdev(major_num, "looper");
+  if (major_num <= 0) {
+    printk(KERN_WARNING "looper: unable to get major number\n");
+    goto out;
+  }
+  /*
+   * And the gendisk structure.
+   */
+  Device.gd = alloc_disk(16);
+  if (!Device.gd)
+    goto out_unregister;
+  Device.gd->major = major_num;
+  Device.gd->first_minor = 0;
+  Device.gd->fops = &looper_ops;
+  Device.gd->private_data = &Device;
+  strcpy(Device.gd->disk_name, "looper0");
+  set_capacity(Device.gd, nsectors);
+  Device.gd->queue = Queue;
+  add_disk(Device.gd);
+  
+  return 0;
+  
+ out_unregister:
+  unregister_blkdev(major_num, "looper");
+ out:
+  vfree(Device.data);
+  return -ENOMEM;
 }
 
 static void __exit looper_exit(void)
 {
-	del_gendisk(Device.gd);
-	put_disk(Device.gd);
-	unregister_blkdev(major_num, "looper");
-	blk_cleanup_queue(Queue);
-	vfree(Device.data);
+  del_gendisk(Device.gd);
+  put_disk(Device.gd);
+  unregister_blkdev(major_num, "looper");
+  blk_cleanup_queue(Queue);
+  vfree(Device.data);
 }
 
 module_init(looper_init);
